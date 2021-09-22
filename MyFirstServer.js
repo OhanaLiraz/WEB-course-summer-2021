1ed31c37eba9633f6a01abc7cd6aa32d64d9c5a1
OhanaLiraz <88513335+OhanaLiraz@users.noreply.github.com>
2021-09-22 17:06:32 +0300

    Add files via upload
    home work 6 commit

--- MyFirstServer.js
+++ MyFirstServer.js
@@ -1,6 +1,37 @@
-var http = require('http');
 
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
-res.end('Hello World!');
-}).listen(8080);
+    const sql = require("./db.js");
+    const mysql = require("mysql2");
+    const express = require("express");
+    const bodyParser = require("body-parser");
+    const app = express();
+
+    // parse requests of contenttype: application/json
+    app.use(bodyParser.json());
+    // parse requests of contenttype: application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true
+    }));
+
+    // simple route
+    app.get("/", (req, res) => {
+    res.json({ message: "Hello World !" });
+    });
+    // set port, listen for requests
+    app.listen(8080, () => {
+    console.log("Server is running on port 3000."
+    );
+    });
+
+
+    // Create a route for getting all customers
+app.get("/table", function(req, res){
+    sql.query("SELECT * FROM myFirstTable", (err, mySQLrespons) => {
+    if (err) {
+    console.log("error: ", err);
+    res.status(400).send({message: "error in getting all records: " + err});
+    return;
+    }
+    console.log("got all records...");
+    res.send(mySQLrespons);
+    return;
+    });
+    });
