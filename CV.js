b306e529ce1a56021cbebddc80ad8ed6e2a34e2d
OhanaLiraz <88513335+OhanaLiraz@users.noreply.github.com>
2021-09-28 22:07:29 +0300

    Delete CV.js

--- CV.js
+++
@@ -1,6 +0,0 @@
-function changeHeader(){
-    document.getElementById("h").innerHTML = " כותרת חדשה לאחר טעינה";
-}
-function initHeader(){
-    document.getElementById("h").innerHTML ="לירז אוחנה - קורות חיים"
-}
